c431a7aa29f8bb70a5c75216402242014164dd55
Yulian Gyurov <yg.atanasoff@gmail.com>
2025-01-04 12:23:03 +0200

    Initial Commit

---
+++ WeeSplit/WeeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeeSplitApp.swift
+//  WeeSplit
+//
+//  Created by Yulian Gyuroff on 4.01.25.
+//
+
+import SwiftUI
+
+@main
+struct WeeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
